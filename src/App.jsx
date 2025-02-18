4a2e0d30dfcedbffc70a45c5b99a4dd98f270efd
Naofel <billhakim07@gmail.com>
2025-02-18 06:53:57 +0600

    first commit of the chat app inbox

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import { Button } from "@/components/ui/button"
+function App() {
+  return (
+    <>
+      <p className="text-3xl p-0 m-0 font-bold">Realtime Chat</p>
+      <Button>Button</Button>
+
+    </>
+  )
+}
+
+export default App
